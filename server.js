05bb39d89aa21e3d7afb4422051ccd40c2b00532
oussama24bessaad <93039035+oussama24bessaad@users.noreply.github.com>
2022-02-15 11:57:34 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const app = express()
 app.use(cors({origin: ['http://localhost:3000']}))
 require('dotenv').config()
 app.use(express.json())
+
 mongoose.connect(process.env.MONGO_URI,(err)=>(
   err ? console.log(err) : console.log('database connected')
-))
+)) 
 
 const User = require('./models/userModel')
 
 
 // routes
 app.use('/api/users',require('./routes/userRoutes'))
-// User.create({name:'safouan',email:'safouan@gmail.com'})
+// User.create({name:'oussama',email:'oussama24bessaad@gmail.com'})
 
 app.listen(process.env.PORT,()=>console.log('server is running on port 5000'))
